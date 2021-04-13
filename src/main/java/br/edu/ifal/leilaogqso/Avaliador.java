6fe829bf268440864fc2cb81f994e72a3b26029c
Josafá Veríssimo <josafaverissimo98@gmail.com>
2021-04-13 16:25:08 -0300

    refactor(avaliador): método avalia refatorado

--- src/main/java/br/edu/ifal/leilaogqso/Avaliador.java
+++ src/main/java/br/edu/ifal/leilaogqso/Avaliador.java
@@ -1,18 +1,31 @@
 package br.edu.ifal.leilaogqso;
 
+import java.util.List;
+
 public class Avaliador {
 
     private double maiorDeTodos = 0;
-    private double menorDeTodos = Double.MAX_VALUE;
+    private double menorDeTodos = 0;
 
     public void avalia(Leilao leilao) {
-        for (Lance lance : leilao.getLances()) {
-            if (lance.getValor() > maiorDeTodos) {
-                maiorDeTodos = lance.getValor();
-            }
-            
-            if (lance.getValor() < menorDeTodos) {
-                menorDeTodos = lance.getValor();
+        final List<Lance> lances = leilao.getLances();
+
+        for(int i = 0; i < lances.size(); i++) {
+            final double lance = lances.get(i).getValor();
+
+            if(i == 0) {
+                if(lance > 0) {
+                    this.maiorDeTodos = lance;
+                    this.menorDeTodos = lance;
+                }
+            } else {
+                if(this.maiorDeTodos < lance) {
+                    this.maiorDeTodos = lance;
+                }
+
+                if(this.menorDeTodos > lance) {
+                    this.menorDeTodos = lance;
+                }
             }
         }
     }
